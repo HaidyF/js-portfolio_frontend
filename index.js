56523fc0672fa6d3b5212d81e7036f3d77a45ce4
HaidyF <69495135+HaidyF@users.noreply.github.com>
2021-04-08 16:16:03 -0400

    fixes

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ function fetchProjects(){
     return fetch(`${BASE_URL}/projects`)
       .then(response => response.json())
       .then(projects => {
+          console.log(projects)
           for (const project of projects){
-            let p = new Project(project.id, project.title, project.quote, project.description, project.image_URL, project.location, project.year, project.category_id)
+            let p = new Project(project.title, project.quote, project.description, project.image_URL, project.location, project.year, project.category_id)
             p.renderProject()
         }
       })
@@ -13,32 +14,13 @@ function fetchProjects(){
 
   document.addEventListener('DOMContentLoaded', () => {
    newForm()
-    fetchProjects()
+    this.fetchProjects()
       .then(results => console.log(results))
   })
 
   //CREATE
   function newForm(){
       let pForm = document.getElementById("projects-form")
-
-      pForm.innerHTML +=
-
-    //   <form>
-    //     <label for="title">Title:</label> 
-    //     <input type="text" id="title"> <br>
-    //     <label for="quote">quote:</label> 
-    //     <input type="text" id"quote"> <br>
-    //     <label for="description">Description:</label> 
-    //     <input type="text" id"description"> <br>
-    //     <label for="image_URL">Image:</label> 
-    //     <input type="text" id"image_URL"> <br>
-    //     <label for="location">Location:</label> 
-    //     <input type="text" id"location"> <br>
-    //     <label for="year">Year Designed:</label> 
-    //     <input type="text" id"year"> <br>
-    //     <input type="submit" value="Create Project"> <br>
-    //   </form>
-
       pForm.addEventListener("submit", pFormSubmit)
 }
 
@@ -48,29 +30,37 @@ function pFormSubmit(){
     let description = document.getElementById("description").value
     let image_URL = document.getElementById("image_URL").value
     let location = document.getElementById("location").value
-    let year = document.getElementById("year").value
-
+    let year = parseInt(document.getElementById("year").value)
+    let category_id = document.getElementById("category_id").value
+    
     let project = {
         title: title, 
         quote: quote,
         description: description,
         image_URL: image_URL,
         location: location,
-        year: year
+        year: year,
+        category_id: category_id
+
     }
     fetch(`${BASE_URL}/projects`, {
-        method: "POST", 
+        method: 'POST',
         headers: {
-            'Accept': 'application/json'
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
+            Accept: "application/json"
         },
         body: JSON.stringify(project)
     })
-    .then(response =>  response.json())
-    .then(project => {
-        let p = new Project(project.id, project.title, project.quote, project.description, project.image_URL, project.location, project.year, project.category_id)
-        p.renderProject()
+
+    .then(response =>  {
+        response.json()
+        console.log(response)
     })
+    .then(json => {
+        console.log(json)
+        window.location.reload()
+    })
+    
 }
   //update
   //delete
--- models/project.js
+++ models/project.js
@@ -1,6 +1,5 @@
 class Project {
-    constructor(id, title, quote, description, image_URL, location, year, category_id) {
-        this.id = id
+    constructor(title, quote, description, image_URL, location, year, category_id) {
         this.title = title
         this.quote = quote
         this.description = description
@@ -9,20 +8,31 @@ class Project {
         this.year = year
         this.category_id = category_id
     }
-}
 
     renderProject(){
         let pDiv = document.getElementById("projects-container")
         pDiv.innerHTML +=
-        <ul>
-        <h2>Project: ${this.title} </h2>
-        <li>
-            ${this.quote}
-            ${this.description}
-            ${this.image_URL}
-            ${this.location}
-            ${this.year}
-        </li>
-    </ul>
+    "<ul>"+
+        "<h2>Project: "+ this.title + "</h2>"+
+        "<li>"+
+            "Quote: "+
+            this.quote+
+            "</li>"+
+            "<li>"+
+            "Description: "+ 
+            this.description+
+            "</li>"+
+            "<li>"+
+            "Project Image: "+
+            "<img src=\""+this.image_URL+"\">"+
+            "</li>"+
+            "<li>"+
+            this.location+
+            "</li>"+
+            "<li>"+
+            this.year+
+        "</li>"+
+   "</ul>"
     {/* <button class ="delete-button" data-id=${this.id}>Delete Project</button> */}
+}
 }
