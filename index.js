f5b121fd1cd8ca032bdabe7a6614284026b1a9df
HaidyF <69495135+HaidyF@users.noreply.github.com>
2021-04-21 00:42:40 -0400

    Seperation of concerns

--- index.js
+++ index.js
@@ -1,69 +1,57 @@
 const BASE_URL = "http://localhost:3000"
 let projectList = document.getElementById('projects-form')
 let allProjects = []
+const projectsAdapter = new ProjectsAdapter
 
 //READ
-function fetchProjects(){
-    return fetch(`${BASE_URL}/projects`)
-      .then(response => response.json())
-      .then(projects => {
-          projects.forEach(
-            project => {
-                let p = new Project(project.id, project.title, project.quote, project.description, project.image_URL, project.location, project.year, project.category_id)
-                p.renderProject()
-                allProjects.push(p)
-            })
-      })
-  }
-
   document.addEventListener('DOMContentLoaded', () => {
    newForm()
-    this.fetchProjects()
+    projectsAdapter.fetchProjects()
   })
 
 //CREATE
   function newForm(){
       let pForm = document.getElementById("projects-form")
-      pForm.addEventListener("submit", pFormSubmit)
+      pForm.addEventListener("submit", projectsAdapter.pFormSubmit)
 }
 
-function pFormSubmit(event){
-    event.preventDefault()
-    let title = document.getElementById("title").value
-    let quote = document.getElementById("quote").value
-    let description = document.getElementById("description").value
-    let image_URL = document.getElementById("image_URL").value
-    let location = document.getElementById("location").value
-    let year = parseInt(document.getElementById("year").value)
-    let category_id = document.getElementById("id").value
+// function pFormSubmit(event){
+//     event.preventDefault()
+//     let title = document.getElementById("title").value
+//     let quote = document.getElementById("quote").value
+//     let description = document.getElementById("description").value
+//     let image_URL = document.getElementById("image_URL").value
+//     let location = document.getElementById("location").value
+//     let year = parseInt(document.getElementById("year").value)
+//     let category_id = document.getElementById("id").value
     
-    let project = {
-        title: title, 
-        quote: quote,
-        description: description,
-        image_URL: image_URL,
-        location: location,
-        year: year,
-        category_id: category_id
-    }
+//     let project = {
+//         title: title, 
+//         quote: quote,
+//         description: description,
+//         image_URL: image_URL,
+//         location: location,
+//         year: year,
+//         category_id: category_id
+//     }
 
-    fetch(`${BASE_URL}/projects`, {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-        },
-        body: JSON.stringify(project)
-    })
+//     fetch(`${BASE_URL}/projects`, {
+//         method: 'POST',
+//         headers: {
+//             "Content-Type": "application/json",
+//             Accept: "application/json"
+//         },
+//         body: JSON.stringify(project)
+//     })
 
-    .then(response =>  {
-        response.json()
-    })
-    .then(json => {
-        console.log(json)
-        window.location.reload()
-    })
-}
+//     .then(response =>  {
+//         response.json()
+//     })
+//     .then(json => {
+//         console.log(json)
+//         window.location.reload()
+//     })
+// }
 
 //DELETE
   function deleteProject(id){
---
+++ models/projectsAdapter.js
@@ -0,0 +1,56 @@
+class ProjectsAdapter{
+    constructor(){
+        this.BASE_URL = "http://localhost:3000/projects"
+    }
+
+    fetchProjects(){
+        fetch(this.BASE_URL)
+        .then(response => response.json())
+        .then(projects => {
+              projects.forEach(
+                project => {
+                    let p = new Project(project.id, project.title, project.quote, project.description, project.image_URL, project.location, project.year, project.category_id)
+                    p.renderProject()
+                    allProjects.push(p)
+                })
+          })
+      }
+
+    pFormSubmit(event){
+        event.preventDefault()
+
+        let title = document.getElementById("title").value
+        let quote = document.getElementById("quote").value
+        let description = document.getElementById("description").value
+        let image_URL = document.getElementById("image_URL").value
+        let location = document.getElementById("location").value
+        let year = parseInt(document.getElementById("year").value)
+        let category_id = document.getElementById("id").value
+        
+        let project = {
+            title, 
+            quote,
+            description,
+            image_URL,
+            location,
+            year,
+            category_id
+        }
+
+        let configObj = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify(project)
+        }
+        
+        fetch("http://localhost:3000/projects", configObj)
+        .then(res => res.json())
+        .then(project => { 
+            console.log(project)
+        window.location.reload()})
+    }
+   
+}
